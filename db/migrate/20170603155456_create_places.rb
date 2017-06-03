78e848fab171b7540c0c9da4d6c5a3e9bfa8a1e2
sherifkhalil <eng_sherifkhalil@hotmail.com>
2017-06-03 20:15:00 +0200

    add seeded data

--- db/migrate/20170603155456_create_places.rb
+++ db/migrate/20170603155456_create_places.rb
@@ -2,8 +2,8 @@ class CreatePlaces < ActiveRecord::Migration[5.0]
   def change
     create_table :places do |t|
       t.string :name
-      t.decimal :longitude
-      t.decimal :latitude
+      t.decimal :longitude, {:precision=>10, :scale=>6}
+      t.decimal :latitude, {:precision=>10, :scale=>6}
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# ruby encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Group.create([{name: 'Axis'}, {name: 'Allies'}])
+
+Place.create([
+    {name: 'Maadi', longitude: 31.2769, latitude: 29.9627},
+    {name: 'Zamalek', longitude: 31.2197, latitude: 30.0609},
+    {name: 'Smart Village', longitude: 31.0178835, latitude: 30.073165},
+    {name: 'New Cairo', longitude: 31.4685, latitude: 30.0342}
+])
